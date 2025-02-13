e4fb4cb0641c6bd19797bcb2acc155ffd8a505ac
islem-habta <167141087+islem-habta@users.noreply.github.com>
2025-02-13 15:16:06 +0100

    Updated route.ts and other files

---
+++ src/app/admin/products/[id]/download/route.ts
@@ -0,0 +1,34 @@
+import db from "@/db/db";
+import { notFound } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs/promises";
+
+export async function GET(req: NextRequest, { params: { id } }) {
+  if (!id || typeof id !== "string") {
+    return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+  }
+
+  const product = await db.product.findUnique({
+    where: { id },
+    select: { filePath: true, name: true },
+  });
+
+  if (product == null) return notFound();
+
+  try {
+    const { size } = await fs.stat(product.filePath);
+    const file = await fs.readFile(product.filePath);
+    const extension = product.filePath.split(".").pop();
+    const safeName = product.name.replace(/[^a-zA-Z0-9-_]/g, "_");
+     //i did this
+    return new NextResponse(file, {
+      headers: {
+        "Content-Disposition": `attachment, filename="${safeName}.${extension}"`,
+        "Content-Length": size.toString(),
+      },
+    });
+  } catch (error) {
+    console.error("File system error:", error);
+    return notFound();
+  }
+}
